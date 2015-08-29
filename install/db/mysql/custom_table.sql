d1a057236837a95d0d9575c3d8d1a75e84f96666
ASGAlex <asgalex@gmail.com>
2015-08-29 20:05:16 +0300

    Зачатки следующего примера. Скорее всего, это не рабочий коммит ;-)

---
+++ install/db/mysql/custom_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `digitalwand_admin_helper_custom_table_demo` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `STRING` VARCHAR(128) NULL,
+  `NUMBER` INT(11) NULL,
+  `TEXT` TEXT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `digitalwand_admin_helper_custom_table_demo_related` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `PARENT_ID` int(11) UNSIGNED NOT NULL,
+  `STRING` INT(11) NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
